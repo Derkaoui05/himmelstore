8545d47ef53176d48640bdd0a3dfe41d58c8bedf
Derkaoui05 <147924188+Derkaoui05@users.noreply.github.com>
2026-08-14 13:10:31 +0100

    fix 404 bas request image error

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,14 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   transpilePackages: ["@himmel/api", "@himmel/db", "@himmel/types"],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "beautysuccess.co",
       },
       {
-        protocol: "http",
-        hostname: "**",
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
   },
